3e5b6405601268f5d8828a15730e0a20429c329f
hou <1374720047@qq.com>
2020-04-06 21:14:41 +0800

    重新添加作品

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
 # 作品展示
 
 ### 个人简历
-<a href="作品/个人简历网站/个人简历.html" target="_blink">个人简历</a>
+<ul>
+    <li>
+        <a href="作品/个人简历网站/个人简历.html" target="_blink">个人简历</a>
+    </li>
+</ul>
 
 ### html+css
 <ul>
---
+++ 作品/个人简历网站/js/animation.js
@@ -0,0 +1,42 @@
+// obj代表需要做动画对象
+// target目标位置
+// callback回调函数
+function animation(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        // 公式: (目标值 - 现在的位置) / 10 得到步长
+        var step = (target - obj.offsetLeft) / 10;
+        // 步长值如果是正数,需要向上取整 如果是负数,就需要向下取整
+        step = step >= 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft == target) {
+            clearInterval(obj.timer);
+            // 动画执行完毕,如果有回调函数就调用回调函数
+            // if (callback){
+            //     callback();
+            // }
+            callback && callback() // 这种写法等同于上面if写法
+        } else {
+            obj.style.left = obj.offsetLeft + step + "px";
+        }
+    }, 15)
+}
+
+function animation_scroll(obj, target, callback) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        // 公式: (目标值 - 现在的位置) / 10 得到步长
+        var step = (target - window.pageYOffset) / 10;
+        // 步长值如果是正数,需要向上取整 如果是负数,就需要向下取整
+        step = step >= 0 ? Math.ceil(step) : Math.floor(step);
+        if (window.pageYOffset == target) {
+            clearInterval(obj.timer);
+            // 动画执行完毕,如果有回调函数就调用回调函数
+            // if (callback){
+            //     callback();
+            // }
+            callback && callback() // 这种写法等同于上面if写法
+        } else {
+            window.scroll(0, window.pageYOffset + step);
+        }
+    }, 15)
+}
